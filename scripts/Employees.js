7e4c4d5250aaf0d634c0eb09e756d704ddf9e198
jkuns76 <jkoonman@gmail.com>
2021-10-14 10:57:31 -0500

    first commit

---
+++ scripts/Employees.js
@@ -0,0 +1,43 @@
+import { getEmployees } from "./database.js"
+import { getOrders } from "./database.js"
+
+const orders = getOrders()
+const employees = getEmployees()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [, employeeId] = itemClicked.id.split("--")
+
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+
+                    const employeeOrders = orders.filter( 
+                        (order) => {
+                            if (order.employeeId === employee.id) {
+                                return true
+                            }
+                        }
+                    )
+
+                    window.alert(` ${employee.name} sold ${employeeOrders.length} products `)
+                }
+            }
+        }
+    }
+)
+
+export const Employees = () => {
+    let html = "<ul>"
+
+    for (const employee of employees) {
+        html += `<li id="employee--${employee.id}">${employee.name}</li>`
+    }
+
+    html += "</ul>"
+
+    return html
+}
+
